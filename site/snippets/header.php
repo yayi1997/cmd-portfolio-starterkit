4845a751196c5add0a11b3dc727dc52345b662e7
yayi1997 <60937271+yayi1997@users.noreply.github.com>
2020-03-31 16:37:23 +0200

    klooien

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!-- Dit is mijn custom stylesheet -->
+  <link rel="stylesheet" href="assets/css/custom.css" type="text/css">
+
+ 
   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
   <link rel="icon" href="<?= $site->image()->url() ?>">
 
